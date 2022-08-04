7d37168c047e6301910377542e4540cfb2cad268
nil-lokh <nilesh.lokhande705@gmail.com>
2022-08-04 14:21:23 +0530

    test

---
+++ demoGitJpa/src/main/java/com/inno/DemoGitJpaApplication.java
@@ -0,0 +1,13 @@
+package com.inno;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemoGitJpaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DemoGitJpaApplication.class, args);
+	}
+
+}
